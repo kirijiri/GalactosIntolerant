3f8c345ef6369c7bcbfb827afaa94c38de7dfc33
kirijiri <kirijiri@gmail.com>
2014-11-08 20:01:21 +0000

    stop and release planet

--- Assets/Scripts/planetTouch.cs
+++ Assets/Scripts/planetTouch.cs
@@ -2,23 +2,30 @@
 using System.Collections;
 
 public class planetTouch : MonoBehaviour {
+	Collider2D hit_collider;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.GetMouseButton (0)) 
-		{
-			Debug.Log ("Pressed left click.");
-			// hold -> stop planet
+		if (Input.GetMouseButtonDown (0) && !hit_collider) {
+			RaycastHit2D hit = Physics2D.Raycast (Camera.main.ScreenToWorldPoint (Input.mousePosition), Vector2.zero);
+			if (hit.collider != null) {
+				//Vector3 targetPos = hit.collider.gameObject.transform.position; //Save the position of the object mouse was over
+				//Destroy (GameObject.Find (hit.collider.gameObject.name));
 
-			// move -> accellerate
+				// stop planet
+				hit.collider.rigidbody2D.isKinematic = true;
+				hit_collider = hit.collider;
+			}
+		}
 
-			// drag to other -> swap
+		if (Input.GetMouseButtonUp (0)) {
+
+			// release planet again
+			if (hit_collider != null) {
+				hit_collider.rigidbody2D.isKinematic = false;
+			}
+			hit_collider = null;
 		}
 	}
 }
