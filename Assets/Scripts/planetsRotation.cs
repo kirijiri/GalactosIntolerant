6042338fdf664970f8b7f4ddeec31a0e2effe86d
kirijiri <kirijiri@gmail.com>
2014-11-08 16:15:30 +0000

    first script for planets

---
+++ Assets/Scripts/planetsRotation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class planetsRotation : MonoBehaviour {
+	public string myName;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("I am alive and my name is " + myName);
+		Debug.Log (transform.position);
+		Debug.Log (transform.parent.position);
+		transform.position = new Vector3(2,2,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
