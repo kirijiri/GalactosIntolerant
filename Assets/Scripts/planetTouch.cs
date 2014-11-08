9f3623d049715ea2f3232ba9936ca6f43fb1df75
kirijiri <kirijiri@gmail.com>
2014-11-08 18:24:05 +0000

    rigid bodies and touch script

---
+++ Assets/Scripts/planetTouch.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class planetTouch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButton (0)) 
+		{
+			Debug.Log ("Pressed left click.");
+			// hold -> stop planet
+
+			// move -> accellerate
+
+			// drag to other -> swap
+		}
+	}
+}
