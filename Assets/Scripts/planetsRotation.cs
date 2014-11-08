fa548867f17572dd805c20e0e68ea4eefdf1c970
TheArbitor <seamantis@hotmail.com>
2014-11-08 17:22:09 +0000

    Rotating planet with variables

--- Assets/Scripts/planetsRotation.cs
+++ Assets/Scripts/planetsRotation.cs
@@ -3,19 +3,24 @@ using System.Collections;
 
 public class planetsRotation : MonoBehaviour {
 	public string myName;
+	public float radius;
+	public float startAngle;
+	public float speed;
+	float angle;
 
 	// Use this for initialization
 	void Start () {
-		Debug.Log("I am alive and my name is " + myName);
-		Debug.Log (transform.position);
-		Debug.Log (transform.parent.position);
-		transform.position = new Vector3(1,1,0);
+		angle = startAngle;
+		SetPosition();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-		transform.position += new Vector3(0.2f,0.2f,0.0f);
-	
+		angle += speed;
+		SetPosition();
+	}
+
+	private void SetPosition () {
+		transform.localPosition = Quaternion.AngleAxis(angle, new Vector3(0,0,1)) * new Vector3(radius,0,0);
 	}
 }
