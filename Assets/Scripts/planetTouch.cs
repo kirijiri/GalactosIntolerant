1ffbb7d00fda60edc8a13e5ff31c5942896fca26
Peter Agg <peter.agg@googlemail.com>
2014-12-14 19:56:58 +0000

    variable names

--- Assets/Scripts/planetTouch.cs
+++ Assets/Scripts/planetTouch.cs
@@ -2,10 +2,11 @@
 using System.Collections;
 
 public class planetTouch : MonoBehaviour {
+	//------------------------------------------------------------------- init
 	//data
 	private Vector3 storedPosition;
 	private Vector3 storedVelocity;
-	private Vector3 offsetVelocity;
+	private Vector3 newVelocity;
 	// debug
 	private debug debugScript;
 	// controls
@@ -15,6 +16,8 @@ public class planetTouch : MonoBehaviour {
 	void Start () {
 		debugScript = (debug)GameObject.Find("debug").GetComponent(typeof(debug));
 	}
+	
+	//------------------------------------------------------------------- event functions
 
 	void OnMouseDown () {
 		// store data
@@ -26,31 +29,21 @@ public class planetTouch : MonoBehaviour {
 	}
 	
 	void OnMouseDrag () {
-		float dist;
-		dist = Vector3.Distance (storedPosition, InputPosition());
+		float dist = Vector3.Distance (storedPosition, InputPosition());
+		float radius = GetComponent<CircleCollider2D> ().radius;
 
-		if (dist < GetComponent<CircleCollider2D>().radius) {
-			held = true;
-			}
+		if (dist < radius) { 
+			held = true; }
 		else{
 			flooked = true;
-			offsetVelocity = InputPosition() - storedPosition;
+			newVelocity = InputPosition() - storedPosition;
 		}
 	}
 
 	void Update () {
 		// flicking control
 		if (flooked && InputReleased()) {
-			float dot = Vector3.Dot(storedVelocity, offsetVelocity);
-
-			// invert the velocity if we're changing direction
-			if (dot > 0){
-				rigidbody2D.velocity = storedVelocity + offsetVelocity;
-			}
-			else {
-				rigidbody2D.velocity = (storedVelocity * -1) + offsetVelocity;
-			}
-
+			rigidbody2D.velocity = newVelocity;
 			ResetControlFlags();
 			return;
 		}
@@ -63,38 +56,14 @@ public class planetTouch : MonoBehaviour {
 		}
 	}
 
+	//------------------------------------------------------------------- private functions
 	private Vector3 InputPosition () {
-		return Camera.main.ScreenToWorldPoint(
-			new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));}
+		Vector3 camPosition =  Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+		camPosition.z = 0;
+		return camPosition;
+	}
 
 	private bool InputReleased(){ return Input.GetMouseButtonUp (0); }
 
 	private void ResetControlFlags(){ held=false; flooked=false; }
 }
-
-
-/*
-void OnMouseDown () {
-	dragOrigin = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
-	dragOriginVelocity = gameObject.rigidbody2D.velocity;	
-}
-
-void OnMouseDrag () {
-	Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
-	Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
-	
-	dragOrigin.z = 0;
-	curPosition.z = 0;
-	Vector3 direction = curPosition - dragOrigin;
-	
-	// debug print/draw
-	if (debugScript.showTouch){
-		//float intensity = direction.magnitude;
-		//Debug.Log ("OnMouseDrag " + dragOrigin + " " + curPosition + " dir: " + direction + " intensity:" + intensity);
-		Debug.DrawLine (dragOrigin, curPosition, Color.white, 3.0f, false);
-	}
-	
-	// add mouse drag as force to planet
-	rigidbody2D.AddForce(new Vector2(direction.x*100, direction.y*100), ForceMode2D.Force);
-}
-*/
