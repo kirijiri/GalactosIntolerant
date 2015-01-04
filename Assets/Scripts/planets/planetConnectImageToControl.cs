0bb71ea132cc415ef374580b6a464b5aa8e9bcac
disusedgenius <peter.agg@googlemail.com>
2015-01-04 10:13:43 +0000

    Planets move in pixel res

--- Assets/Scripts/planets/planetConnectImageToControl.cs
+++ Assets/Scripts/planets/planetConnectImageToControl.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 
 public class planetConnectImageToControl : MonoBehaviour {
 	public GameObject parent;
+	public bool smooth = false;
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +12,17 @@ public class planetConnectImageToControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = parent.transform.position;
+		Vector3 pos = parent.transform.position;
+		if (smooth) 
+		{
+			transform.position = pos;
+		} 
+		else 
+		{
+			// rounding to 2 DP due to pixel ratio in images
+			float x = (float)Math.Round (pos.x, 2);
+			float y = (float)Math.Round (pos.y, 2);
+			transform.position = new Vector3 (x, y, 0);
+		}
 	}
 }
