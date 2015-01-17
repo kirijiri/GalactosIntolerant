a9a02045dfaa18088a8bcfbb79dd40b933e9c047
kirijiri <kirijiri@gmail.com>
2015-01-17 21:08:29 +0000

    score screen gets a 'restart the game' button

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -2,10 +2,12 @@
 using System.Collections;
 
 public class gameManager : MonoBehaviour {
+
+	// globals
 	public int aligned_planet_count = 0;
 
-	// Use this for initialization
-	void Start () {
+	void Awake () {
+		Debug.Log ("new gameManager");
 		DontDestroyOnLoad(this);
 	}
 }
---
+++ Assets/Scripts/restart.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CircleCollider2D))]
+
+public class restart : MonoBehaviour {
+
+	// Use this for initialization
+	void OnMouseDown () {
+		Application.LoadLevel ("titleScreen");
+	}
+}
