e7baccb276e3c2ed55b78c00064b8fcb3d62b9b9
kirijiri <kirijiri@gmail.com>
2015-03-08 01:49:34 +0000

    white Rect on screen for phone message

--- Assets/Scripts/phone/phoneAnimation.cs
+++ Assets/Scripts/phone/phoneAnimation.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class phoneAnimation : MonoBehaviour {
 
+    private Rect restingPosition;
+
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+        restingPosition = new Rect(80, 140, 235, 50);
+    }
+    
+    void OnGUI()
+    {
+        Texture2D tx2DFlash = new Texture2D(1, 1); //Creates 2D texture
+        tx2DFlash.SetPixel(1, 1, Color.white); //Sets the 1 pixel to be white
+        tx2DFlash.Apply(); //Applies all the changes made
+        print (tx2DFlash);
+        GUI.DrawTexture(restingPosition, tx2DFlash); //Draws the texture for the entire screen (width, height)
 	}
 }
