58f7559cab23266453a29db77e429f21c8122729
kirijiri <kirijiri@gmail.com>
2015-03-15 19:20:39 +0000

    snapshot flash fade in

--- Assets/Scripts/snapshot.cs
+++ Assets/Scripts/snapshot.cs
@@ -8,11 +8,17 @@ using System.Collections;
 public class snapshot : MonoBehaviour
 {
     private gravityBeam gravityBeam;
+    private bool doSnapshot = false;
+    private float time;
+    private float fadeInSpeed;
+
+    private tinker tinker;
 
     //-------------------------------------------------------------------
 
     void Start()
     {
+        tinker = GameObject.Find("tinker").GetComponent<tinker>();
         gravityBeam = GameObject.Find("gravityBeam").GetComponent<gravityBeam>();
         
         // make button invisible (gravity beam will make it visible)
@@ -26,11 +32,41 @@ public class snapshot : MonoBehaviour
 
         if (!gravityBeam.isActive && gravityBeam.available) 
             return;
-
+        
         // save stats
         gameManager.Instance.alignedPlanetCount = gravityBeam.GetAlignedPlanetCount();
 
+        // animate flash
+        doSnapshot = true;
+        StartCoroutine(WaitAndScoreScreen());
+    }
+
+    IEnumerator WaitAndScoreScreen(){
+        time = Time.time;
+        yield return new WaitForSeconds(0.25f);
+
         // switch screens
         sceneManager.GoToScoreScreen();
+        yield break;
+    }
+
+    void Update()
+    {
+        fadeInSpeed = tinker.flashFadeInSpeed;
+    }
+
+    void OnGUI()
+    {
+        if (!doSnapshot) return;
+
+        Rect screenRect = new Rect(0, 0, Screen.width, Screen.height);
+
+        Texture2D tx;
+        tx = new Texture2D(1, 1);          
+        Color lerpedColor = Color.Lerp(Color.clear, Color.white, (Time.time - time)*fadeInSpeed);
+        tx.SetPixel(1, 1, lerpedColor);    
+        tx.Apply();                        
+
+        GUI.DrawTexture(screenRect, tx); 
     }
 }
