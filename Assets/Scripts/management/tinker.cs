63e16c5bfe3abcdf9f048fbc7ad88374ef027a82
Peter Agg <peter.agg@googlemail.com>
2015-03-29 16:35:52 +0100

    default shake in tinker

--- Assets/Scripts/management/tinker.cs
+++ Assets/Scripts/management/tinker.cs
@@ -41,7 +41,7 @@ public class tinker : MonoBehaviour
     public float GBMinGuideDistance = 30f;
     public float GBMaxGuideDistance = 64f;
     public float GBShakeAmount = 0.01f;
-    public float GBBackDropShakeAmount = 0.5f;
+    public float GBBackDropShakeAmount = 2.0f;
     public float GBDragShakeFraction = 0.25f;
 
     // messages
