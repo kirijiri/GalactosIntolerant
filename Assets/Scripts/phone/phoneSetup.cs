76796ff3a78527d868ef36a441c742e797785a1f
kirijiri <kirijiri@gmail.com>
2015-09-06 01:40:50 +0100

    fitted boxes with text finally

--- Assets/Scripts/phone/phoneSetup.cs
+++ Assets/Scripts/phone/phoneSetup.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -6,8 +7,9 @@ public class phoneSetup : MonoBehaviour
 {
 	private Camera _cam;
 	private RenderTexture _tex;
-	private GUIText _text;
-	private Rect _sizeRect;
+	private GUIStyle _textStyle;
+	private Text _textComponent;
+	private float _height;
 	
 	private List<string> messages = new List<string>();
 	
@@ -27,8 +29,8 @@ public class phoneSetup : MonoBehaviour
 		// Create a new camera to use, copying from the main camera
 		_cam = (Camera)Camera.Instantiate (
 			original.GetComponent<Camera> (), 
-			new Vector3 (0, 0, 0), 
-			Quaternion.FromToRotation (new Vector3 (0, 0, 0), new Vector3 (0, 0, 1)));
+			new Vector3 (0, 0, -10), 
+			Quaternion.FromToRotation (new Vector3 (0, 0, 1), new Vector3 (0, 0, 1)));
 		
 		// Set the background of the new one and disable audioListener
 		_cam.backgroundColor = Color.green;
@@ -44,64 +46,70 @@ public class phoneSetup : MonoBehaviour
 		_cam.cullingMask = (1 << LayerMask.NameToLayer ("renderTexCam"));
 		
 		// create new render texture
-		_tex = new RenderTexture (256, 256, 32, RenderTextureFormat.ARGB32);
+		_tex = new RenderTexture (1024, 1024, 32, RenderTextureFormat.ARGB32);
 		_tex.Create ();
 		_cam.targetTexture = _tex;
 		
+		// textStyle
+		_textStyle = new GUIStyle();
+		_textStyle.normal.textColor = Color.black;
+		_textStyle.wordWrap = true;
+		_textStyle.fontSize = 22;
+		_textStyle.font = (Font)Resources.Load("MunroSmall");
+
+		// canvas
+		GameObject canvas = new GameObject ("Canvas");
+		canvas.layer = LayerMask.NameToLayer ("renderTexCam");
+		canvas.AddComponent<Canvas> ();
+		Canvas myCanvas = canvas.GetComponent<Canvas> ();
+		myCanvas.renderMode = RenderMode.ScreenSpaceCamera;
+		myCanvas.worldCamera = _cam;
+
 		// text
-		GameObject bananas = new GameObject ("text");
-		_text = bananas.AddComponent<GUIText> ();
-		_text.transform.position = new Vector3 (0.5f, 1.0f, 0f);
-		_text.color = Color.black;
-		
-		// put into custom layer as well
-		bananas.layer = LayerMask.NameToLayer ("renderTexCam");
-		
+		GameObject myText = new GameObject ("Text");
+		myText.layer = LayerMask.NameToLayer ("renderTexCam");
+		myText.AddComponent<RectTransform> ();
+		myText.AddComponent<CanvasRenderer> ();
+		myText.AddComponent<Text> ();
+
+		RectTransform _textRect = myText.GetComponent<RectTransform> ();
+		_textRect.localScale = new Vector3(1, 1, 1);
+		myText.transform.parent = myCanvas.transform;
+		_textRect.localPosition = new Vector3(-_tex.width/2+90/2, 0, 0);
+		_textRect.sizeDelta = new Vector2(90, _tex.height);
+
+		_textComponent = myText.GetComponent<Text> ();
+		_textComponent.font = _textStyle.font;
+		_textComponent.fontSize = _textStyle.fontSize;
+
 		// update text every couple of seconds
 		StartCoroutine(UpdateNews());
 	}
 	
 	IEnumerator UpdateNews ()
 	{
-		string message = messages[Random.Range(0, messages.Count)];
-		print (message);
-
 		_cam.backgroundColor = new Color(Random.value, Random.value, Random.value);
-
-		GUIContent content = new GUIContent(message);
-		
-		GUIStyle textStyle = new GUIStyle();
-
-		//int fontSize = 500; // random number of how big I guess it should be on screen (will be relative to screen size then anyway
-		//textStyle.fontSize = Mathf.Min(Mathf.FloorToInt(256 * fontSize/1000), Mathf.FloorToInt(200 * fontSize/1000));
-		//_text.fontSize = textStyle.fontSize;
-
-		_text.fontSize = Mathf.RoundToInt (100.0f * 256.0f / (256.0f * 1.0f));
-		
-		// align top center
-		_text.anchor = TextAnchor.LowerLeft;
-		
-		// center is 900/2
-		//_sizeRect = new Rect (_tex.width/2 - 900/2, 0,  900, 200);
-		_sizeRect = new Rect (0, 0, _tex.width, 200);
-		print (_sizeRect);
-		
-		((GUIText)_text.GetComponent<GUIText> ()).text = message;
-		
-		GUI.Label(_sizeRect, "GEWFWQWDQWDW", textStyle);
 
 		//yield return new WaitForEndOfFrame(); // fix for: "ReadPixels was called to read pixels from system frame buffer, while not inside drawing frame."
 		Texture2D tx2d = new Texture2D (_tex.height, _tex.width, TextureFormat.RGB24, false);
 		RenderTexture.active = _tex;
-		tx2d.ReadPixels (_sizeRect, 0, 0);
+		Rect sizeRect = new Rect (0, 0, 90, _height);
+		tx2d.ReadPixels (sizeRect, 0, 0);
 		tx2d.Apply ();
 		RenderTexture.active = null;
 		
 		SpriteRenderer sr = new GameObject ("note").AddComponent<SpriteRenderer> ();
-		sr.sprite = Sprite.Create (tx2d, _sizeRect, new Vector2 (0.5f, 0.5f));
+		sr.sprite = Sprite.Create (tx2d, sizeRect, new Vector2 (0.5f, 0.5f));
+
+		// prepare next idle message
+		string message = messages[Random.Range(0, messages.Count)];
+		_textComponent.text = message;
+
+		_height = _textStyle.CalcHeight(new GUIContent (message), 90);
+		_textStyle.fontSize = _textComponent.fontSize;
+		print ("\n" + message + ":  " + _height);
 		
-		yield return new WaitForSeconds(1);
-		//yield return new WaitForEndOfFrame();
+		yield return new WaitForSeconds(2);
 		StartCoroutine( UpdateNews() );
 	}
-}
+}	
