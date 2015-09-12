4ddd034a04e46d1303e87ec3d4ccd6253cbefcaf
Claudia Zimmer <kirijiri@gmail.com>
2015-09-12 17:42:41 +0100

    lets start again

---
+++ Assets/Scripts/phone/phoneBullshit.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class phoneBullshit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		print (Application.dataPath);
+		print (Application.dataPath + "/Resources/");
+		string[] files = System.IO.Directory.GetFiles (Application.dataPath + "/Resources/");
+		print(files);
+
+		DirectoryInfo dir = new DirectoryInfo(Application.dataPath + "/Resources");
+		print (dir);
+		FileInfo[] info = dir.GetFiles("*.*");
+		print (info);
+		/*string fullNames = info.Select(f => f.FullName).ToArray();
+		foreach (string f in info) 
+		{
+			print(f);
+		}
+		*/
+
+		Texture2D[] textures = (Texture2D[]) Resources.LoadAll("Textures");
+		print (textures);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
